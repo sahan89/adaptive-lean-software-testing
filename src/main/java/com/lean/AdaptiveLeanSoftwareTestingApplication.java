f6bf6ec4da3724803cf3742eb0a180a6dafe67ec
sahan89 <sahan1989@gmail.com>
2025-05-01 21:31:05 +0200

    Initial commit

---
+++ src/main/java/com/lean/AdaptiveLeanSoftwareTestingApplication.java
@@ -0,0 +1,13 @@
+package com.lean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptiveLeanSoftwareTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaptiveLeanSoftwareTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lean/AdaptiveLeanSoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptiveLeanSoftwareTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
